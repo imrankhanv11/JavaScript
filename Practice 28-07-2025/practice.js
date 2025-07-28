02ed673876862f5819cc10d1ca663cd1ad919d64
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-07-28 13:44:39 +0530

    Add files via upload

--- Practice 28-07-2025/practice.js
+++ Practice 28-07-2025/practice.js
@@ -1,1 +1,24 @@
+//display
 console.log("hello world"); 
+
+//calculate the date differance
+let start = new Date("2025-07-01");
+let end = new Date("2025-07-12");
+
+let diff = end.getTime() - start.getTime();
+let days = diff / (1000*60*60*24);
+
+console.log(days);
+
+//date somes
+let date1 = new Date();
+console.log(date1);
+
+let date2 = new Date();
+console.log(date2.getFullYear());
+
+console.log(date2.getDate());
+
+console.log(date2.getDay());
+
+console.log(date2.getMonth());
