1f7a176c4ea0c8fc693e5711c35af9dca75e5de7
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-07-28 17:47:22 +0530

    Add files via upload

---
+++ Practice 28-07-2025/Operators.js
@@ -0,0 +1,49 @@
+let a = 10;
+let b = 3;
+
+//arithmatic
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(a ** b);
+a++;
+console.log(a);
+b--;
+console.log(b);
+
+//assignment
+let x = 5;
+
+x += 3;
+console.log(x);
+
+x -= 2;
+console.log(x);
+
+x %= 3;
+console.log(x);
+
+x *= 4;
+console.log(x);
+
+x /= 6;
+console.log(x);
+
+x **= 2;
+console.log(x);
+
+
+//comparison
+let num1 = 7;
+let num2 = "7";
+
+console.log(num1 == num2);
+console.log(num1 === num2);
+console.log(num1 != num2);
+console.log(num1 !== num2);
+console.log(num1 < 5);
+console.log(num1 > 5);
+console.log(num1 >= 7);
+console.log(num1 <= 6);
