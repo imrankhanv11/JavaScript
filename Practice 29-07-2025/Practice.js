fe0eed84141c5f6aede4028b9cf00c517a3e8264
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-07-29 09:43:06 +0530

    Add files via upload

--- Practice 29-07-2025/Practice.js
+++ Practice 29-07-2025/Practice.js
@@ -4,3 +4,28 @@
     console.log(stringvalue.charCodeAt(3));
     console.log(stringvalue.codePointAt(3));
 }
+
+let marks = 75;
+if (marks >= 90) {
+  console.log("Grade A");
+} else if (marks >= 75) {
+  console.log("Grade B");
+} else {
+  console.log("Grade C");
+}
+
+let day = "Wednesday";
+
+switch (day) {
+  case "Monday":
+    console.log("Start of the week");
+    break;
+  case "Wednesday":
+    console.log("Midweek");
+    break;
+  case "Friday":
+    console.log("Weekend is near");
+    break;
+  default:
+    console.log("Just another day");
+}
