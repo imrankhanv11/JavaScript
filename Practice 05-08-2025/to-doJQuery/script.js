194f759247b29b784129678ebbab5a3072310dd5
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-08-05 11:49:40 +0530

    Add files via upload

--- Practice 05-08-2025/to-doJQuery/script.js
+++ Practice 05-08-2025/to-doJQuery/script.js
@@ -30,44 +30,6 @@ $('document').ready(function () {
         }
     });
 
-    showtask();
-
-    async function showtask() {
-
-        let output = $('#output');
-
-        try {
-            const response = await fetch(url);
-
-            if (!response) {
-                throw new Error('Fetch is failed');
-            }
-
-            const Data = await response.json();
-            output.empty();
-
-            Data.forEach(element => {
-                output.append(goodview(element));
-            });
-
-        }
-        catch (error) {
-            console.log(error);
-        }
-    }
-
-    function goodview(data) {
-        const div = $('<div>').addClass('taskdiv').data('id', data.id);
-
-        const divt = $('<div>').addClass('tdiv').text(data.ToDo);
-        const del = $('<button>').text('Delete').data('btn', data.id).addClass('delbtn');
-
-        div.append(divt).append(del);
-
-        return div;
-    }
-
-
     async function showtask() {
         let output = $('#output');
         output.empty(); // Clear existing items
@@ -89,7 +51,7 @@ $('document').ready(function () {
     }
 
     function goodview(data) {
-        const div = $('<div>').addClass('taskdiv').attr('data-id', data.id);
+        const div = $('<div>').addClass('taskdiv');
 
         const divText = $('<div>').addClass('tdiv').text(data.ToDo);
 
@@ -101,6 +63,8 @@ $('document').ready(function () {
         div.append(divText).append(del);
         return div;
     }
+    
+    showtask();
 
     // Delete button
     $('#output').on('click', '.delBtn', async function (e) {
